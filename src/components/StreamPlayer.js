71f1212db1f2fa63ce622d32059b3b6cddd4f824
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-03-12 13:53:35 +0100

    Removed width and height. Video.js is fluid and follows parent containers size

--- src/components/StreamPlayer.js
+++ src/components/StreamPlayer.js
@@ -1,6 +1,6 @@
-import React, { useEffect, forwardRef } from 'react'
-import Videojs from 'video.js'
-import 'video.js/dist/video-js.css';
+import React, { useEffect, forwardRef } from "react";
+import Videojs from "video.js";
+import "video.js/dist/video-js.css";
 
 export default forwardRef(function StreamPlayer(props, ref) {
     const src = props.props.src;
@@ -10,24 +10,22 @@ export default forwardRef(function StreamPlayer(props, ref) {
         autoplay: false,
         muted: true,
         controls: true,
-        width: 600,
-        heigh: 400,
     };
 
     useEffect(() => {
         const player = Videojs(ref.current, options, () => {
-            player.src({ type: 'video/mp4', src: src });
+            player.src({ type: "video/mp4", src: src });
             player.poster(poster);
         });
 
-        return (() => {
+        return () => {
             player.dispose();
-        })
-    }, [])
+        };
+    }, []);
 
     return (
         <div data-vjs-player>
-            <video ref={ref} className="video-js vjs-16-9" playsInline />
+            <video ref={ref} className='video-js vjs-16-9' playsInline />
         </div>
-    )
+    );
 });
