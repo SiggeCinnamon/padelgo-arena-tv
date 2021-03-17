b2887af0b2c501fc2ae0d839460dddc016a281ac
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-03-17 09:57:20 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
 import './App.css';
-import Clubs from './componets/Clubs'
-import Courts from './componets/Courts'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/clubs">clubs</Link>
-            </li>
-            <li>
-              <Link to="/court">courts</Link>
-            </li>
-            <li>
-              <Link to="/">home</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/clubs">
-            <Clubs />
-          </Route>
-          <Route path="/court">
-            <Courts />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+    </div>
   );
 }
 
