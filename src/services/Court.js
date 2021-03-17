0b653ef7920a509146e2a5b237f7b8170d82e6a0
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-03-17 09:33:41 +0100

    Added Court and Streams services for reusable axios calls

---
+++ src/services/Court.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+async function getCourtsAPIGETResponse(route) {
+    try {
+        // setDebugLevel(1);
+
+        const config = {
+            baseURL: 'https://staging-courts.padelgo.tv/',
+            method: 'GET',
+        }
+
+        try {
+            const result = await axios.create(config).get(route);
+            return result.data;
+        }
+        catch (error) {
+            if (typeof (error.response) !== 'undefined' && error.response.status === 404) {
+                console.error('Got 404 response from court API');
+                return false;
+            }
+            else {
+                console.log('Generic error in court API. we should log this.');
+                console.error(error);
+                throw error;
+            }
+        }
+    } catch (error) {
+        console.log('There was an error calling the court API.');
+        console.error(error);
+    }
+}
+
+export async function getCourtsWithClubId(clubId) {
+    return await getCourtsAPIGETResponse(`Courts/${clubId}`);
+}
---
+++ src/services/Streams.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+async function getStreamsAPIGETResponse(route) {
+    try {
+
+        const config = {
+            baseURL: 'https://staging-streams.padelgo.tv/',
+            method: 'GET',
+        }
+
+        try {
+            const result = await axios.create(config).get(route);
+            return result.data;
+        }
+        catch (error) {
+            if (typeof (error.response) !== 'undefined' && error.response.status === 404) {
+                console.error('Got 404 response from court API');
+                return false;
+            }
+            else {
+                console.log('Generic error in court API. we should log this.');
+                console.error(error);
+                throw error;
+            }
+        }
+    } catch (error) {
+        console.log('There was an error calling the court API.');
+        console.error(error);
+    }
+}
+
+export async function getStreamsWithCourtId(courtId) {
+    return await getStreamsAPIGETResponse(`Streams/court/${courtId}`);
+}
+
+export async function getStreamURLWithLiveStreamId(liveStreamId) {
+    return await getStreamsAPIGETResponse(`Streams/url/anonymously/${liveStreamId}`);
+}
+
+export async function getStreamThumbnailWithLiveStreamId(liveStreamId) {
+    return await getStreamsAPIGETResponse(`Streams/${liveStreamId}/thumbnail`);
+}
