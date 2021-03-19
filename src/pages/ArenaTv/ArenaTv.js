0d80303e654f8b690142c416c04677bb956dab27
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-03-19 14:46:51 +0100

    Renamed Highlights to ArenaTv
    Added a sort of pipeline to change how the array and object looks when passed to playlist.
    Added a onPlaylistAtEnd function that is passed along as a prop to the Videoplayer.
    Added jsx for VideoPlayer component
    
    Modified so that the pipeline checks current node environment, big buck bunny is the bomb.

--- src/pages/ArenaTv/ArenaTv.js
+++ src/pages/ArenaTv/ArenaTv.js
@@ -1,32 +1,81 @@
 import React, { useState, useEffect, useRef } from "react";
 import { getPopularMedia } from "../../services/Media.js";
-import { debugMsg, setDebugLevel } from "simplistic-log";
+import VideoPlayer from "../../components/StreamPlayer/VideoPlayer.js";
 
-function Highlights(props) {
+function ArenaTv(props) {
   const [popular, setPopular] = useState([]);
+  const [sources, setSources] = useState([]);
+  const playerRef = useRef(null);
 
   useEffect(() => {
-    fetchPopularMedia();
+    fetchPopularMedia(1, 5);
   }, []);
 
-  const fetchPopularMedia = async () => {
-    setPopular(
-      await getPopularMedia({
-        clubId: props.clubId,
-        stream: false,
-        liveStream: false,
-        highlight: true,
-        video: false,
-        page: 1,
-        take: 10,
-        sortOrder: 0,
-      })
-    );
+  useEffect(() => {
+    setSources(pipeline(popular));
+  }, [popular]);
 
-    debugMsg(popular, 0);
+  const pipeline = (data) => {
+    const pipeline = [];
+
+    data.forEach((p) => {
+      pipeline.push({
+        sources: [
+          {
+            src:
+              process.env.NODE_ENV === "development"
+                ? "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+                : p.url,
+            type: "video/mp4",
+          },
+        ],
+        poster:
+          process.env.NODE_ENV === "development"
+            ? "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/800px-Big_Buck_Bunny_thumbnail_vlc.png"
+            : p.poster,
+      });
+    });
+
+    return pipeline;
   };
 
-  return <div className='container'>Arena tv</div>;
+  const onPlaylistAtEnd = async () => {
+    const data = pipeline(await fetchPopularMedia(1, 5));
+    setSources(sources.concat(data));
+  };
+
+  const fetchPopularMedia = async (page = 1, take = 0, sortOrder = 0) => {
+    const data = await getPopularMedia({
+      clubId: Number(props.location.props.clubId),
+      stream: true,
+      liveStream: false,
+      highlight: false,
+      video: false,
+      page: page,
+      take: take,
+      sortOrder: sortOrder,
+    });
+
+    setPopular(await data);
+    return await data;
+  };
+
+  return (
+    <div className='container'>
+      <h1>Arena tv</h1>
+      <div className='player'>
+        {sources && (
+          <VideoPlayer
+            ref={playerRef}
+            src={sources}
+            controls={true}
+            autoplay={false}
+            onPlaylistAtEnd={onPlaylistAtEnd}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Highlights;
+export default ArenaTv;
