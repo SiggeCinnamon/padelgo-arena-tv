b6522a07554b9833c744a6f62fec693469d1840f
SiggeCinnamon <mattias.andersen@gmail.com>
2021-03-23 14:07:03 +0100

    Csssssss....

--- src/pages/Court/Court.js
+++ src/pages/Court/Court.js
@@ -1,42 +1,41 @@
-import React, { useState, useEffect } from 'react';
-
-import { getStreamsWithCourtId } from '../../services/Streams.js';
-import { getScoresWithLiveStreamId } from '../../services/Scores.js';
-import Scoreboard from '../../components/ScoreBoard/scoreboard.js';
+import React, { useState, useEffect } from "react";
+import { getStreamsWithCourtId } from "../../services/Streams.js";
+import { getScoresWithLiveStreamId } from "../../services/Scores.js";
+import Scoreboard from "../../components/ScoreBoard/scoreboard.js";
 import "./Court.css";
 function Court(props) {
-  const [score, setScore] = useState("");
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      fetchScore();
-    }, 5000);
-    return () => clearInterval(interval);
-  }, []);
+    const [score, setScore] = useState("");
 
     useEffect(() => {
         const interval = setInterval(() => {
-            console.log('5sek')
             fetchScore();
-        }, 50000);
+        }, 5000);
         return () => clearInterval(interval);
     }, []);
 
     useEffect(() => {
-        fetchScore()
-    }, [])
+        fetchScore();
+    }, []);
 
     const fetchScore = async () => {
-        const FetchGetStreamsWithCourtId = await getStreamsWithCourtId(props.match.params.courtId)
-        setScore(await getScoresWithLiveStreamId(FetchGetStreamsWithCourtId[0]?.liveStreamId || ""));
+        const FetchGetStreamsWithCourtId = await getStreamsWithCourtId(
+            props.match.params.courtId
+        );
+        setScore(
+            await getScoresWithLiveStreamId(
+                FetchGetStreamsWithCourtId[0]?.liveStreamId || ""
+            )
+        );
     };
 
     return (
-        <div className="wrapper">
+        <>
             <Scoreboard score={score} />
-        </div>
-    )
+        </>
+    );
 }
-export default Court
+
+export default Court;
+
 //Fetch https://staging-streams.padelgo.tv/Streams/court/{courtId} to get "liveStreamId"
 //Fetch https://staging-scores.padelgo.tv//Scores/{liveStreamId} to get score
