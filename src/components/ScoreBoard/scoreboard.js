cb1654510fca04502ff9fb7eee4a8cdade45ebb4
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-03-23 13:17:01 +0100

    Changed wrapper name to wrapperScoreboard cause of conflicts Removed unused comments

--- src/components/ScoreBoard/scoreboard.js
+++ src/components/ScoreBoard/scoreboard.js
@@ -1,47 +1,36 @@
 import ScoreboardTeam from "./ScoreBoardTeam";
 import styles from "./scoreboard.module.css";
 
+// TODO: Fix responsiveness at mobile resolutions (Iphone X)
+
 export default function Scoreboard({ isManageScorePage, score }) {
   if (!score || score.error) {
     return null;
   } else {
     return (
-      <div className={styles.wrapper}>
-        <div
-          id='scores-container'
-          className={
-            !isManageScorePage
-              ? styles.scoresContainer
-              : styles.scoresContainerManageScore
-          }>
-          {score.isLive && (
-            <div className={styles.liveBubble}>
-              <span>LIVE</span>
-            </div>
-          )}
-          <div className={styles.test1}>
-            <div className={styles.container}>
-              <ScoreboardTeam team={score.team[0]} />
-              <ScoreboardTeam team={score.team[1]} />
+      <>
+        <div className={styles.wrapperScoreboard}>
+          <div
+            id='scores-container'
+            className={
+              !isManageScorePage
+                ? styles.scoresContainer
+                : styles.scoresContainerManageScore
+            }>
+            {score.isLive && (
+              <div className={styles.liveBubble}>
+                <span>LIVE</span>
+              </div>
+            )}
+            <div className={styles.test1}>
+              <div className={styles.container}>
+                <ScoreboardTeam team={score.team[0]} />
+                <ScoreboardTeam team={score.team[1]} />
+              </div>
             </div>
           </div>
-          <br />
-          <br />
-          <br />
-          {/* <div className={styles.test2}>
-                        <div className={styles.container}>
-                            <ScoreboardTeam team={score.team[0]} />
-                            <ScoreboardTeam team={score.team[1]} />
-                        </div>  </div>
-                    <br /><br /><br />
-                    <div className={styles.test3}>
-                        <div className={styles.container}>
-                            <ScoreboardTeam team={score.team[0]} />
-                            <ScoreboardTeam team={score.team[1]} />
-                        </div>
-                    </div> */}
         </div>
-      </div>
+      </>
     );
   }
 }
