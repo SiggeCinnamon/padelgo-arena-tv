eff438f7578ae7fc012aa0cbe68c9dcd34ceb811
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-03-23 11:22:01 +0100

    Fixed where I accidentally screwed up the last merge with a single }

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -15,55 +15,55 @@ function Dashboard({ match }) {
 
   const fetchCourts = async () => {
     setCourts(await getCourtsWithClubId(match.params.clubId));
-
-    const toArenaTv = () => {
-      return {
-        pathname: Routes.ARENA_TV,
-        props: { clubId: match.params.clubId },
-      };
-    };
-
-    return (
-      <>
-        <div className='vertical-center'>
-          <div className='btn-div'>
-            <Link className='btn btn-primary' to={toArenaTv}>
-              Arena TV
-            </Link>
-          </div>
-          <div className='btn-group' role='group' aria-label='Court selector'>
-            <div className='select-div'>
-              <select
-                className='form-select'
-                aria-label='Default select example'
-                value={court}
-                onChange={(e) => {
-                  setCourt(e.currentTarget.value);
-                }}>
-                <option value='-1' style={{ fontWeight: "bold" }}>
-                  Courts
-                </option>
-                <option disabled='disabled'>--------</option>
-                {courts &&
-                  courts.map((c, i) => (
-                    <option value={c.courtId} key={c.courtId}>
-                      Court #{c.courtId}
-                    </option>
-                  ))}
-              </select>
-            </div>
-            <Link
-              className={
-                court === "-1" ? "btn btn-primary disabled" : "btn btn-primary"
-              }
-              to={`${Routes.COURT.replace(":courtId", court)}`}>
-              Go to court
-            </Link>
-          </div>
-        </div>
-      </>
-    );
   };
+
+  const toArenaTv = () => {
+    return {
+      pathname: Routes.ARENA_TV,
+      props: { clubId: match.params.clubId },
+    };
+  };
+
+  return (
+    <>
+      <div className='vertical-center'>
+        <div className='btn-div'>
+          <Link className='btn btn-primary' to={toArenaTv}>
+            Arena TV
+          </Link>
+        </div>
+        <div className='btn-group' role='group' aria-label='Court selector'>
+          <div className='select-div'>
+            <select
+              className='form-select'
+              aria-label='Default select example'
+              value={court}
+              onChange={(e) => {
+                setCourt(e.currentTarget.value);
+              }}>
+              <option value='-1' style={{ fontWeight: "bold" }}>
+                Courts
+              </option>
+              <option disabled='disabled'>--------</option>
+              {courts &&
+                courts.map((c, i) => (
+                  <option value={c.courtId} key={c.courtId}>
+                    Court #{c.courtId}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <Link
+            className={
+              court === "-1" ? "btn btn-primary disabled" : "btn btn-primary"
+            }
+            to={`${Routes.COURT.replace(":courtId", court)}`}>
+            Go to court
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
