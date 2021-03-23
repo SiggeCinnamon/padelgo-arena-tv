daf4d4527475ea025a0f3a24731c68e1e37c752e
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-03-23 13:18:50 +0100

    Added margin to the player colours Changed Tile colour and transparency

--- src/components/ScoreBoard/ScoreBoardTeam.js
+++ src/components/ScoreBoard/ScoreBoardTeam.js
@@ -1,49 +1,57 @@
-import styles from './scoreboard.module.css';
+import styles from "./scoreboard.module.css";
 
 export default function scoreboardTeam({ team }) {
-    return (
-
-        <div className={styles.teamContainer}>
-            <div className='players-color d-flex p-2' style={{ backgroundColor: team.color }}></div>
-            <div className={styles.playersName} style={{ backgroundColor: team.backgroundColor }}>
-                <span className={styles.teamName}>{team.name}</span>
+  return (
+    <div className={styles.teamContainer}>
+      <div
+        className='players-color d-flex p-2'
+        style={{ backgroundColor: team.color, marginRight: "3px" }}></div>
+      <div
+        className={styles.playersName}
+        style={{ backgroundColor: team.backgroundColor }}>
+        <span className={styles.teamName}>{team.name}</span>
+      </div>
+      <div className='players-score d-flex'>
+        {team.sets.map((set, index) => {
+          return (
+            <div
+              key={index}
+              className={styles.playersScore}
+              style={{ backgroundColor: getScoreTileBackgroundColor(set) }}>
+              <span>{set.game}</span>
             </div>
-            <div className='players-score d-flex'>
-                {
-                    team.sets.map((set, index) => {
-                        return (
-                            <div key={index} className={styles.playersScore} style={{ backgroundColor: getScoreTileBackgroundColor(set) }}>
-                                <span>{set.game}</span>
-                            </div>);
-                    })
-                }
-                <div className={styles.playersScore} style={{ backgroundColor: getCurrentPointTileBackground(team.currentPoint) }}>
-                    <span>{team.currentPoint.score}</span>
-                </div>
-            </div>
+          );
+        })}
+        <div
+          className={styles.playersScore}
+          style={{
+            backgroundColor: getCurrentPointTileBackground(team.currentPoint),
+          }}>
+          <span>{team.currentPoint.score}</span>
         </div>
-
-    );
-};
+      </div>
+    </div>
+  );
+}
 
 const getScoreTileBackgroundColor = (set) => {
-    if (set.isCompleted && set.isWon) {
-        return '#e91e63';
-    }
+  if (set.isCompleted && set.isWon) {
+    return "#e91e63";
+  }
 
-    if (!set.isCompleted && set.isLead) {
-        return '#000000';
-    }
+  if (!set.isCompleted && set.isLead) {
+    return "#000000";
+  }
 
-    return defaultTileColor;
-}
+  return defaultTileColor;
+};
 
 const getCurrentPointTileBackground = (currentPoint) => {
-    if (currentPoint.isLead) {
-        return '#000000';
-    }
+  if (currentPoint.isLead) {
+    return "#000000";
+  }
 
-    return defaultTileColor;
-}
+  return defaultTileColor;
+};
 
-const defaultTileColor = 'rgba(61, 61, 61, 0.3)';
+const defaultTileColor = "rgba(102, 100, 100, 0.50)";
