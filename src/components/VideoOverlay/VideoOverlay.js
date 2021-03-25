715b53c5d30a79a6d4d6198633d9863d3f59ccee
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-03-25 14:25:16 +0100

    Added progress bar on the bottom Added a "pill" that informs the viewer of what type the video is. e.g. Highlight Added a useEffect to change our state depending on media type Added a proper default avatar taken from static-webb

--- src/components/VideoOverlay/VideoOverlay.js
+++ src/components/VideoOverlay/VideoOverlay.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import styles from "./VideoOverlay.module.css";
-import defaultAvatar from "../../assets/default_avatar.svg";
 
-const VideoOverlay = ({ data, history }) => {
+const VideoOverlay = ({ data, currentProgress, history }) => {
+  const [icon, setIcon] = useState("");
+  const [colour, setColour] = useState("");
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    switch (String(data.mediaType).toLowerCase()) {
+      case "stream": {
+        setIcon("movie_filter");
+        setColour("#eb6531");
+        setText("STREAM");
+        break;
+      }
+      case "highlight": {
+        setIcon("live_tv");
+        setColour("#ebab0c");
+        setText("HIGHLIGHT");
+        break;
+      }
+      case "live": {
+        setIcon("stop_circle");
+        setColour("#28b869");
+        setText("LIVE");
+        break;
+      }
+      default: {
+        setIcon("");
+        setColour("#9c27b0");
+        setText("");
+        break;
+      }
+    }
+  }, [icon, colour, data]);
+
   return (
     <>
       <div className={styles.__video_overlay_logo}>
@@ -14,10 +46,23 @@ const VideoOverlay = ({ data, history }) => {
           padelgo.tv - stream for free
         </p>
       </div>
+      {data && data.mediaType && (
+        <div className={styles.__video_overlay_channel_type}>
+          <div
+            className={styles.__video_overlay_channel_type_pill}
+            style={{ background: colour }}>
+            <i className='material-icons media-card--highlight'>{icon}</i>
+            <p>{text}</p>
+          </div>
+        </div>
+      )}
       <div className={styles.__video_overlay_channel}>
         <div className={styles.__video_overlay_channel_content}>
           <img
-            src={data.avatar || defaultAvatar}
+            src={
+              data.avatar ||
+              "https://static.padelgo.tv/profilepictures/600x600/default.jpeg?cache=1616674604690"
+            }
             className='img-raised rounded-circle img-fluid'
             alt='player'
           />
@@ -29,6 +74,16 @@ const VideoOverlay = ({ data, history }) => {
           </div>
         </div>
       </div>
+      {currentProgress && (
+        <div className={styles.__video_overlay_progress_container}>
+          <div className={styles.__video_overlay_progress + " progress"}>
+            <div
+              className={styles.__video_overlay_progress_bar + " progress-bar"}
+              role='progressbar'
+              style={{ width: currentProgress + "%" } || 0 + "%"}></div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
