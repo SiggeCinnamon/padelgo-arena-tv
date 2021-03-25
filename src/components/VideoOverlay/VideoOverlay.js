add1a4f757d9a8df674db8969085b9c8d634c003
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-03-25 08:48:43 +0100

    misc

--- src/components/VideoOverlay/VideoOverlay.js
+++ src/components/VideoOverlay/VideoOverlay.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./VideoOverlay.module.css";
 
+// TODO Test the overlay throughougly by using different channels
 const VideoOverlay = ({ data }) => {
   return (
     <>
