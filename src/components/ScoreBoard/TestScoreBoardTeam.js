da0e0ca985eee63cc8ed60041af6707dd51a8b4e
SiggeCinnamon <mattias.andersen@gmail.com>
2021-03-26 14:36:13 +0100

    asd

--- src/components/ScoreBoard/TestScoreBoardTeam.js
+++ src/components/ScoreBoard/TestScoreBoardTeam.js
@@ -2,58 +2,61 @@ import styles from "./testscoreboard.module.scss";
 import avatar from "./avatar.jpeg";
 import avatar2 from "./avatar2.jpg";
 
-
 export default function scoreboardTeam({ team }) {
   return (
     <>
-
-      <div className={styles.teamcontainer} >
+      <div className={styles.teamcontainer}>
         <div
           className={
             styles.scoreboardcontainer + " d-flex justify-content-around"
           }
         >
-          <div className="col">
-            <img className={styles.avatar} src={avatar}></img>
+          <div className="d-flex">
+            <img className={styles.avatar}  src={avatar}></img>
+            
+            <img className={styles.avataroverlap } src={avatar2}></img>
           </div>
-          <div className="col">
-            <img className={styles.avataroverlap} src={avatar2}></img>
-          </div>
-          <div className={styles.scorecontainer  + " d-flex justify-content-aroud"}>
           <div
-            className={styles.playername + " d-flex justify-content-around"}
-            style={{ backgroundColor: team.backgroundColor }}
+            className={styles.scorecontainer + " d-flex justify-content-aroud"}
           >
-         
-              <span>{team.name}</span>
-
-          </div>
-
-          <div
-            className={styles.setcontainer + " d-flex justify-content-around"}
-          >
-            {team.sets.map((set, index) => {
-              return (
-                <div
-                  key={index}
-                  className={styles.gamecontainer}
-                  style={{
-                    backgroundColor: getScoreTileBackgroundColor(set),
-                  }}
-                >
-                  <span>{set.game}</span>
-                </div>
-              );
-            })}
             <div
-              className={styles.currentscore}
-              style={{backgroundColor: getCurrentPointTileBackground(team.currentPoint),}}>
-              <span>{team.currentPoint.score}</span>
+              className={styles.playername + " d-flex justify-content-around"}
+              style={{ backgroundColor: team.backgroundColor }}
+            >
+              <span>{team.name}</span>
+            </div>
+
+            <div
+              className={styles.setcontainer + " d-flex justify-content-around"}
+            >
+              {team.sets.map((set, index) => {
+                return (
+                  <div
+                    key={index}
+                    className={styles.gamecontainer}
+                    style={{
+                      backgroundColor: getScoreTileBackgroundColor(set),
+                    }}
+                  >
+                    <span>{set.game}</span>
+                 
+                  </div>
+                );
+              })}
+              <div
+                className={styles.currentscore}
+                style={{
+                  backgroundColor: getCurrentPointTileBackground(
+                    team.currentPoint
+                  ),
+                }}
+              >
+                <span>{team.currentPoint.score}</span>
+              </div>
             </div>
           </div>
         </div>
-        </div>
-        </div>
+      </div>
     </>
   );
 }
