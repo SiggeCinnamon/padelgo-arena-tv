7bc42826061707a88c6679320fe329a75d8013eb
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-03-29 13:47:17 +0200

    Added a utility that allows you to turn a object, string or other into SHA hash

---
+++ src/utilities/HashGen.js
@@ -0,0 +1,10 @@
+export default async function Hash(args) {
+  const encodeParam = new TextEncoder().encode(args);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", encodeParam);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+
+  return hashHex;
+}
