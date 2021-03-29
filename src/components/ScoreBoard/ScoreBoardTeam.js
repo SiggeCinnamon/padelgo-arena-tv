fe37df7fd1b90e042708fc4ceb9783b97384af3f
SiggeCinnamon <mattias.andersen@gmail.com>
2021-03-29 15:24:30 +0200

    WIP

--- src/components/ScoreBoard/ScoreBoardTeam.js
+++ src/components/ScoreBoard/ScoreBoardTeam.js
@@ -1,10 +1,49 @@
 import styles from "./scoreboard.module.scss";
 import avatar from "./avatar.jpeg";
 import avatar2 from "./avatar2.jpg";
+import React, { useState, useEffect } from "react";
 
-export default function scoreboardTeam({ team, nameColor }) {
+export default function ScoreboardTeam({
+  team,
+  nameColor,
+  channelName,
+  width,
+}) {
+  console.log(channelName[0].channelName,'bild1');
+  console.log(channelName[1].channelName,'bild2');
+  const [image, setImage] = useState();
+  
+  const setImageByChannelAndWidth = (channelName, width) => {
+    switch (width) {
+      case 64:
+        setImage(
+          `https://static.padelgo.tv/profilepictures/64x64/${channelName}.jpeg?cache=${new Date().getTime()}`
+        );
+        break;
+      case 200:
+        setImage(
+          `https://static.padelgo.tv/profilepictures/200x200/${channelName}.jpeg?cache=${new Date().getTime()}`
+        );
+        break;
+      default:
+        setImage(
+          `https://static.padelgo.tv/profilepictures/600x600/${channelName}.jpeg?cache=${new Date().getTime()}`
+        );
+        break;
+    }
+  };
+  useEffect(() => {
+    setImageByChannelAndWidth(channelName, width);
+  }, [channelName]);
   return (
     <>
+      <div className="thumbnail img-circle">
+        <img
+          className="img-raised"
+          onError={() => setImageByChannelAndWidth("default", width)}
+          alt={channelName}
+        />
+      </div>
       <div className="container justify-center">
         <div className={styles.scoreboardcontainer + "d-flex"}>
           <div className={styles.scorecontainer}>
@@ -22,7 +61,10 @@ export default function scoreboardTeam({ team, nameColor }) {
             </div>
             <div
               className={styles.playername + " d-flex"}
-              style={{ backgroundColor: nameColor === 0 ? "#3D3D3D" : "#FFF", color: nameColor===0 ? "#FFF" : "#3D3D3D"}}
+              style={{
+                backgroundColor: nameColor === 0 ? "#3D3D3D" : "#FFF",
+                color: nameColor === 0 ? "#FFF" : "#3D3D3D",
+              }}
             >
               <span>{team.name}</span>
             </div>
@@ -75,11 +117,22 @@ const getScoreTileBackgroundColor = (set) => {
 };
 
 const getCurrentPointTileBackground = (currentPoint) => {
-  if (currentPoint.isLead) {
-    return "#000000";
-  }
-
   return defaultTileColor;
 };
 
 const defaultTileColor = "rgba(102, 100, 100, 0.50)";
+
+{
+  /* <div className={styles.imgcontainer + "d-flex"}>
+<img
+  className={styles.avatar}
+  style={{ border: "5px solid " + team.color }}
+  src={avatar}
+></img>
+<img
+  className={styles.avataroverlap}
+  style={{ border: "5px solid " + team.color }}
+  src={avatar2}
+></img>
+</div> */
+}
