57d1b63392f15092805348cf4506e0f839567339
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-04-13 10:38:44 +0200

    Added functionality for adding LiveStreams to the sources along other types

--- src/hooks/usePipeline.js
+++ src/hooks/usePipeline.js
@@ -1,25 +1,40 @@
 import { useState, useEffect } from "react";
+import { getStreamURLWithLiveStreamId } from "../services/Streams.js";
 
 const usePipeline = (popular) => {
   const [sources, setSources] = useState([]);
 
   useEffect(() => {
-    setSources(pipeline(popular));
+    const pipeIt = async () => {
+      const pt = await pipeline(popular);
+      setSources(pt);
+    };
+
+    pipeIt();
   }, [popular]);
 
-  const pipeline = (data) => {
+  const pipeline = async (data) => {
     const pipeline = [];
 
-    data.forEach((p) => {
+    for (const d of data) {
+      const stream =
+        d.mediaType === "LiveStream"
+          ? await getStreamURLWithLiveStreamId(d.internalId)
+          : "";
+
       pipeline.push({
         sources: [
           {
             src:
               process.env.NODE_ENV === "development"
                 ? "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-                : p.url,
+                : d.mediaType === "LiveStream"
+                ? stream.url
+                : d.url,
             type:
-              p.mediaType === "LiveStream"
+              process.env.NODE_ENV === "development"
+                ? "video/mp4"
+                : d.mediaType === "LiveStream"
                 ? "application/x-mpegURL"
                 : "video/mp4",
           },
@@ -27,13 +42,13 @@ const usePipeline = (popular) => {
         poster:
           process.env.NODE_ENV === "development"
             ? "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Big_Buck_Bunny_thumbnail_vlc.png/800px-Big_Buck_Bunny_thumbnail_vlc.png"
-            : p.thumbnailURL,
-        mediaType: p.mediaType,
-        channel: p.channel,
-        description: p.description,
-        avatar: `https://static.padelgo.tv/profilepictures/600x600/${p.channel}.jpeg`,
+            : d.thumbnailURL,
+        mediaType: d.mediaType,
+        channel: d.channel,
+        description: d.description,
+        avatar: `https://static.padelgo.tv/profilepictures/600x600/${d.channel}.jpeg`,
       });
-    });
+    }
 
     return pipeline;
   };
