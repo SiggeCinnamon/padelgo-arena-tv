34475ad048d9d49b1d48f81e58b55f2c0a486c55
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-04-13 11:58:21 +0200

    Componentified the Text card in Dashboard
    Removed unused imports

---
+++ src/components/TextCard/TextCard.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./TextCard.module.scss";
+
+const TextCard = ({ textHeader, textBody, linkTo }) => {
+  console.log(textHeader, "\n", textBody, "\n", linkTo);
+
+  return (
+    <div className={styles.__dashboard_card + " card"}>
+      <div className={styles.__dashboard_card_top + " card-top"}>
+        <p>{textHeader}</p>
+      </div>
+      <div className='card-body'>
+        <div>
+          <p
+            className={styles.__dashboard_card_text + " card-text"}
+            style={{ whiteSpace: "pre-line" }}>
+            {textBody}
+          </p>
+        </div>
+      </div>
+      <div className={styles.__dashboard_div_btn}>
+        <Link
+          type='button'
+          className={styles.__dashboard_btn + " btn btn-rounded"}
+          to={linkTo}>
+          Start
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default TextCard;
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { Link, withRouter } from "react-router-dom";
+import React, { useEffect } from "react";
+import { withRouter } from "react-router-dom";
 import Routes from "../../routes.json";
 import styles from "./Dashboard.module.scss";
 import NavBar from "../../components/NavBar";
 import useFetchCourts from "../../hooks/useFetchCourts.js";
 import useFetchClub from "../../hooks/useFetchClub.js";
+import TextCard from "../../components/TextCard";
+import CourtDropCard from "../../components/CourtDropCard";
 
 function Dashboard({ match, history }) {
-  const [court, setCourt] = useState("-1");
   const [courts, setCourts] = useFetchCourts(match.params.clubId);
   const [club, setClub] = useFetchClub(match.params.clubId);
 
@@ -34,90 +35,22 @@ function Dashboard({ match, history }) {
     <>
       <NavBar />
       <div className={styles.__dashboard_container + " container-fluid"}>
-        {/* ARENATV */}
-        <div className={styles.__dashboard_card + " card"}>
-          <div className={styles.__dashboard_card_top + " card-top"}>
-            <p>Showcase</p>
-          </div>
-          <div className='card-body'>
-            <div>
-              <p className={styles.__dashboard_card_text + " card-text"}>
-                Continuously display highlights and streams from your club in
-                fullscreen.
-                <br />
-                <br />
-                Return here by clicking at padelgo.tv in the top left or press
-                ESC.
-              </p>
-            </div>
-          </div>
-          <div className={styles.__dashboard_div_btn}>
-            <Link
-              type='button'
-              className={styles.__dashboard_btn + " btn btn-rounded"}
-              to={`${Routes.ARENA_TV.replace(":clubId", match.params.clubId)}`}>
-              Start
-            </Link>
-          </div>
-        </div>
-        {/* ARENATV */}
+        <TextCard
+          textHeader='Showcase'
+          textBody={`Continuously display highlights and streams from your club in fullscreen.
 
-        {/* SCORE */}
-        <div className={styles.__dashboard_card + " card"}>
-          <div className={styles.__dashboard_card_top + " card-top"}>
-            <p>Score</p>
-          </div>
-          <div className='card-body'>
-            <p className={styles.__dashboard_card_text + " card-text"}>
-              Display the current score from a live stream. Pick a court in the
-              list below.
-            </p>
-          </div>
+                Return here by clicking at padelgo.tv in the top left or press ESC.`}
+          linkTo={Routes.ARENA_TV.replace(":clubId", match.params.clubId)}
+        />
 
-          {/* DROPDOWNMENU */}
-          <div className={styles.__dashboard_dropdown}>
-            <div className='btn-group' role='group' aria-label='Court selector'>
-              <div className='select-div'>
-                <select
-                  className='form-select'
-                  aria-label='Default select example'
-                  value={court}
-                  onChange={(e) => {
-                    setCourt(e.currentTarget.value);
-                  }}>
-                  <optgroup className={styles.__dashboard_optgroup}>
-                    <option value='-1' style={{ fontWeight: "bold" }}>
-                      Courts
-                    </option>
-                    <option disabled='disabled'>--------</option>
-                    {courts &&
-                      courts.map((c, i) => (
-                        <option value={c.courtId} key={c.courtId}>
-                          Court #{c.courtId} at {club.name}
-                        </option>
-                      ))}
-                  </optgroup>
-                </select>
-              </div>
-            </div>
-          </div>
-          {/* DROPDOWNMENU */}
-          {/* COURTBUTTON */}
-          <div className={styles.__dashboard_div_btn}>
-            <Link
-              className={
-                court === "-1"
-                  ? styles.__dashboard_btn + " btn btn-rounded disabled"
-                  : styles.__dashboard_btn + " btn btn-rounded"
-              }
-              to={`${Routes.COURT.replace(":courtId", court)}`}>
-              Start
-            </Link>
-          </div>
-          {/* COURTBUTTON */}
-        </div>
+        <CourtDropCard
+          textHeader='Score'
+          textBody='Display the current score from a live stream. Pick a court in the
+              list below.'
+          pOptions={courts}
+          pAlt={club}
+        />
       </div>
-      {/* SCORE */}
     </>
   );
 }
