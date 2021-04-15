aa977604eb79ec873d24d109c1224370024e0a48
SiggeCinnamon <mattias.andersen@gmail.com>
2021-04-15 11:22:38 +0200

    Added two new componets out of ScoreBoardTeam

--- src/components/ScoreBoard/ScoreBoardTeam.js
+++ src/components/ScoreBoard/ScoreBoardTeam.js
@@ -1,5 +1,7 @@
 import styles from "./ScoreBoard.module.scss";
 import AvatarCircle from "./AvatarCircle/AvatarCircle.js";
+import PlayerName from "./PlayerName/PlayerName.js";
+import SetContainer from "./SetContainer/SetContainer.js";
 
 /**
  * A component that is rendering the AvatarCircle sub-component and also renders the jsx that represents the teams name and their current scores
@@ -13,7 +15,7 @@ import AvatarCircle from "./AvatarCircle/AvatarCircle.js";
 export default function ScoreboardTeam({ team, nameColor, players }) {
   return (
     <>
-      <div className='container justify-center'>
+      <div className="container justify-center">
         <div className={styles.scoreboardContainer + " d-flex"}>
           <div className={styles.scoreContainer}>
             <div className={styles.imgContainer + " d-flex"}>
@@ -32,63 +34,11 @@ export default function ScoreboardTeam({ team, nameColor, players }) {
                 />
               </div>
             </div>
-            <div
-              className={styles.playerName + " d-flex"}
-              style={{
-                backgroundColor: nameColor === 0 ? "#3D3D3D" : "#FFF",
-                color: nameColor === 0 ? "#FFF" : "#3D3D3D",
-              }}
-            >
-              <span>{team.name}</span>
-            </div>
-            <div className={styles.setContainer + " d-flex"}>
-              {team.sets.map((set, index) => {
-                return (
-                  <div
-                    key={index}
-                    className={styles.gameContainer}
-                    style={{
-                      backgroundColor: getScoreTileBackgroundColor(set),
-                    }}
-                  >
-                    <span>{set.game}</span>
-                  </div>
-                );
-              })}
-              <div
-                className={styles.currentScore}
-                style={{
-                  backgroundColor: getCurrentPointTileBackground(team.currentPoint),
-                }}
-              >
-                <span>{team.currentPoint.score}</span>
-              </div>
-            </div>
+            <PlayerName teamName={team.name} nameColor={nameColor} />
+            <SetContainer team={team} />
           </div>
         </div>
       </div>
     </>
   );
 }
-
-const getScoreTileBackgroundColor = (set) => {
-  if (set.isCompleted && set.isWon) {
-    return "#e91e63";
-  }
-
-  if (set.isCompleted || set.isWon) {
-    return "#000000";
-  }
-
-  if (!set.isCompleted && set.isLead) {
-    return "#000000";
-  }
-
-  return defaultTileColor;
-};
-
-const getCurrentPointTileBackground = (currentPoint) => {
-  return defaultTileColor;
-};
-
-const defaultTileColor = "rgba(102, 100, 100, 0.50)";
