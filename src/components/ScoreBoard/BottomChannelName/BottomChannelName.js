17012f12afe0d020c972d95f93374ef3f501e125
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-04-15 13:48:09 +0200

    Misc

--- src/components/ScoreBoard/BottomChannelName/BottomChannelName.js
+++ src/components/ScoreBoard/BottomChannelName/BottomChannelName.js
@@ -4,9 +4,7 @@ import { getChannelsInfoWithChannelName } from "../../../services/Channels.js";
 import useFetchChannel from "../../../hooks/useFetchChannel";
 
 export default function BottomChannelName({ channels }) {
-    const [channel, setChannel] = useFetchChannel(channels[1].channelName);
-
-
+  const [channel, setChannel] = useFetchChannel(channels[0].channelName);
 
   return (
     <>
@@ -15,12 +13,12 @@ export default function BottomChannelName({ channels }) {
           {channel && (
             <img
               src={`https://static.padelgo.tv/profilepictures/600x600/${channel.name}.jpeg`}
-              className="img-raised rounded-circle img-fluid"
+              className='img-raised rounded-circle img-fluid'
               onError={(e) => {
                 e.target.onerror = null;
                 e.target.src = "https://static.padelgo.tv/profilepictures/600x600/default.jpeg";
               }}
-              alt="player"
+              alt='player'
             />
           )}
           <div className={styles.__bottomchannelname_overlay_channel_content_headers}>
