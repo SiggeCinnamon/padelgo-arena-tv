8c47bb280615dd8b9a111df2924e6db0978a0e26
Christoffer Hansen <chris.hansen.ch@outlook.com>
2021-04-16 14:35:54 +0200

    Added shuffling of livestreams and highlighs

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -24,11 +24,22 @@ const Player = ({ clubId }) => {
     await fetchPopularMedia(1, 20);
   };
 
-  const fetchPopularMedia = async (page = 1, take = 0, sortOrder = 0) => {
-    const data = await getPopularMedia({
+  const fetchPopularMedia = async (page = 1, take = 20, sortOrder = 0) => {
+    const liveStreamData = await getPopularMedia({
       clubId: Number(clubId),
       stream: false,
       liveStream: true,
+      highlight: false,
+      video: false,
+      page: page,
+      take: take,
+      sortOrder: sortOrder,
+    });
+
+    const highlightData = await getPopularMedia({
+      clubId: Number(clubId),
+      stream: false,
+      liveStream: false,
       highlight: true,
       video: false,
       page: page,
@@ -36,7 +47,27 @@ const Player = ({ clubId }) => {
       sortOrder: sortOrder,
     });
 
-    setPopular(await data);
+    setPopular(liveStreamData.length > 0 ? await spliceData(liveStreamData, highlightData) : highlightData);
+  };
+
+  const spliceData = async (streamData, highData) => {
+    let splicedData = [];
+    let diff = Math.round(highData.length / streamData.length);
+    let x = 0;
+    let y = 0;
+
+    for (let i = 0; i < highData.length; i++) {
+      if (i === y) {
+        splicedData.push(streamData[x]);
+        splicedData.push(highData[i]);
+        x++;
+        y += diff;
+      } else {
+        splicedData.push(highData[i]);
+      }
+    }
+
+    return splicedData;
   };
 
   return (
