119cadc80bb70c2ec10d938ad68531e9b35628cd
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-04-21 16:34:33 +0200

    Added club name in navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,7 @@
+import react, { useState, useEffect } from "react";
 import { withRouter } from "react-router";
 import styles from "./NavBar.module.scss";
+import { getClubDataWithClubId } from "../../services/Clubs.js";
 
 /**
  * A component that is an overlay with absolute setting, consisting of one link that brings you back to the page you were on previously
@@ -8,16 +10,36 @@ import styles from "./NavBar.module.scss";
  * @param  {Function} history A react-router function that allows us to send the user back to previous pages
  * @return {JSX} React JSX Rendering
  */
-const NavBar = ({ history }) => {
+const NavBar = ({ clubId = -1, history }) => {
+  const [clubName, setClubName] = useState();
+
+  useEffect(() => {
+    const fetchClubData = async () => {
+      const clubData = await getClubDataWithClubId(clubId);
+
+      setClubName(clubData.name);
+    };
+
+    if (clubId && clubId !== -1) {
+      fetchClubData();
+    }
+  }, [clubId]);
+
   return (
     <>
       <nav className={styles.__NavBar_bar}>
-        <p
-          onClick={() => {
-            history.goBack();
-          }}>
-          padelgo.tv - stream for free
-        </p>
+        <div className={styles.__NavBar_title}>
+          <p
+            onClick={() => {
+              history.goBack();
+            }}
+          >
+            padelgo.tv - stream for free
+          </p>
+        </div>
+        <div className={styles.__NavBar_clubName}>
+          <p>{clubName}</p>
+        </div>
       </nav>
     </>
   );
