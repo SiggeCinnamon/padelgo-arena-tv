54c1c8bc0f3e7814464aefbbd234298717aefe90
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-05-07 15:26:15 +0200

    Removed clubId as prop argument Removed clubId as prop in NavBar tag
    Added clubName prop in NavBar tag that takes value from new metadata property

--- src/components/VideoOverlay/VideoOverlay.js
+++ src/components/VideoOverlay/VideoOverlay.js
@@ -8,15 +8,15 @@ import TypePill from "./TypePill.js";
  * The main component of VideoOverlay that handles the rendering of all the sub-components and the required conditionals
  * @author Christoffer Hansen
  *
- * @param  {Number} clubId A Number representing the club that the user picked from Home page
+ * @param  {String} clubName A String representing the clubs name
  * @param  {Object} data An object consisting of data of the current source
  * @param  {Number} currentProgress A number representing the current % amount played of the video/stream
  * @return {JSX} React JSX Rendering
  */
-const VideoOverlay = ({ clubId, data, currentProgress }) => {
+const VideoOverlay = ({ data, currentProgress }) => {
   return (
     <>
-      <NavBar clubId={clubId} />
+      <NavBar clubName={data.clubName} />
       {data && data.mediaType && <TypePill data={data} />}
       {data && <BottomOverlay data={data} />}
       {currentProgress && <ProgressbarOverlay currentProgress={currentProgress} />}
