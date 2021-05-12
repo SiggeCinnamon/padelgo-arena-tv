61d0cbe0e19227d5394a8294a7bcd2e07fb5f41d
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-05-12 16:53:34 +0200

    test misc

--- src/utilities/GTM/index.js
+++ src/utilities/GTM/index.js
@@ -1,11 +1,11 @@
 export default function sendEventToGTM(name, data) {
-  console.log("GTM:", process.env.REACT_APP_GTM_ACTIVE);
-  if (typeof window !== "undefined" && process.env.REACT_APP_GTM_ACTIVE === true) {
-    console.log("I AM IN BRUH!!!!!!?! WOOP WOOP");
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({
-      event: name,
-      data: data
-    });
+  if (typeof window !== "undefined") {
+    if (process.env.REACT_APP_GTM_ACTIVE) {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: name,
+        data: data
+      });
+    }
   }
 }
