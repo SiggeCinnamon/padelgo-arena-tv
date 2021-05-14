3d6925f6a101942e9ccde70a871869fbab03d4cc
Christoffer Hansen <chris.hansen.ch@outlook.com>
2021-05-14 13:08:55 +0200

    Added a temporary testpage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Court from "./pages/Court";
 import Dashboard from "./pages/Dashboard";
 import ArenaTv from "./pages/ArenaTv";
+import TestPage from "./pages/TestPage";
 import TagManager from "react-gtm-module";
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Route path={Routes.COURT} component={Court} />
         <Route path={Routes.DASHBOARD} component={Dashboard} />
         <Route path={Routes.ARENA_TV} component={ArenaTv} />
+        <Route path={Routes.TEST} component={TestPage} />
       </Switch>
     </Router>
   );
