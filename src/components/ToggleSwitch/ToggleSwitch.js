cbe327b8c2251e3f8e5f39c09db7d0fe3ebf73e0
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-05-17 10:43:32 +0200

    Major changes to ToggleSwitch
    Added PropTypes
    Added default values by new standards
    Added defaultChecked to input checkbox

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,23 +1,26 @@
-import React, { useEffect } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import styles from "./ToggleSwitch.module.scss";
 
-const ToggleSwitch = ({ name, value, onChange, dataYes, dataNo }) => {
-  
-    const onClick = (e) => {
-        if (onChange) {
-            onChange();
-        }
+const ToggleSwitch = ({ name = "__ToggleSwitch_checkbox", value = true, onChange, dataYes = "Yes", dataNo = "No" }) => {
+  const onClick = (e) => {
+    if (onChange) {
+      onChange();
     }
+  };
 
-    useEffect(() => {
-        console.log("TS.value:", value);
-    }, [value]);
-  
-    return (
+  return (
     <>
       <div className={styles.toggle_switch}>
-        <input type="checkbox" className={styles.toggle_switch_checkbox} name={name} id={name} value={value} onChange={onClick}/>
+        <input
+          type="checkbox"
+          className={styles.toggle_switch_checkbox}
+          defaultChecked={value}
+          name={name}
+          id={name}
+          value={value}
+          onChange={onClick}
+        />
         <label className={styles.toggle_switch_label} htmlFor={name}>
           <span className={styles.toggle_switch_inner} data-yes={dataYes} data-no={dataNo} />
           <span className={styles.toggle_switch_switch} />
@@ -27,11 +30,11 @@ const ToggleSwitch = ({ name, value, onChange, dataYes, dataNo }) => {
   );
 };
 
-ToggleSwitch.defaultProps = {
-    name: "__ToggleSwitch_checkbox",
-    value: 0,
-    dataYes: "Yes",
-    dataNo: "No"
-}
+ToggleSwitch.propTypes = {
+  name: PropTypes.string,
+  value: PropTypes.bool,
+  dataYes: PropTypes.string,
+  dataNo: PropTypes.string
+};
 
 export default ToggleSwitch;
