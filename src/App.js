1690a89c68944645d595a0cf877366225393a746
HansenChristoffer <chris.hansen.ch@outlook.com>
2021-05-17 10:45:40 +0200

    Removed unused TestPage route

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import Home from "./pages/Home";
 import Court from "./pages/Court";
 import Dashboard from "./pages/Dashboard";
 import ArenaTv from "./pages/ArenaTv";
-import TestPage from "./pages/TestPage";
 import TagManager from "react-gtm-module";
 
 function App() {
   const tagManagerArgs = {
-    gtmId: "GTM-P9R6PDC"
+    gtmId: process.env.REACT_APP_GTM_ID
   };
 
   TagManager.initialize(tagManagerArgs);
@@ -21,7 +20,6 @@ function App() {
         <Route path={Routes.COURT} component={Court} />
         <Route path={Routes.DASHBOARD} component={Dashboard} />
         <Route path={Routes.ARENA_TV} component={ArenaTv} />
-        <Route path={Routes.TEST} component={TestPage} />
       </Switch>
     </Router>
   );
